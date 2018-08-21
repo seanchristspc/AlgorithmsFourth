3f7e3a6f6b9422c8413c63396ad611a73129de08
seanchristspc <seanchristspc@gmail.com>
2018-08-21 17:26:04 +0800

    happy

---
+++ src/com/sean/fundamentals/Wget.java
@@ -0,0 +1,16 @@
+package com.sean.fundamentals;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+
+public class Wget{
+    public static void main(String[] args){
+        String url=args[0];
+        In in=new In(url);
+        String data=in.readAll();
+        String filename=url.substring(url.lastIndexOf("/")+1);
+        Out out=new Out(filename);
+        out.println(data);
+        out.close();
+    }
+}
---
+++ src/com/sean/fundamentals/readme.txt
@@ -0,0 +1,10 @@
+下载数据文件地址:
+
+
+largeT.txt
+
+https://algs4.cs.princeton.edu/11model/largeT.txt
+
+largeW.txt
+https://algs4.cs.princeton.edu/11model/largeW.txt
+
